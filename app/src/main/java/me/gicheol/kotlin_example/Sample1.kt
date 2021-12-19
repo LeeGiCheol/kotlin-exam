5e25801b6c4a658ec48ee1d4fc767dc337f5504c
LeeGiCheol <asdqwek200@naver.com>
2021-12-19 21:26:14 +0900

    Array, List

--- app/src/main/java/me/gicheol/kotlin_example/Sample1.kt
+++ app/src/main/java/me/gicheol/kotlin_example/Sample1.kt
@@ -1,57 +1,19 @@
 package me.gicheol.kotlin_example
 
 fun main() {
-    println(max(1, 5))
-    println(max2(5, 10))
-    println(max3(10, 15))
-    checkNum(1)
-    checkNum2(1)
-    checkNum3(1)
 }
 
-fun max(a : Int, b : Int) : Int {
-    if (a > b) {
-        return a
-    } else {
-        return b
-    }
-}
+// List, MutableList
+// List 는 Immutable 이지만 Array, ArrayList 는 Mutable 이다
+fun array() {
+    val array = arrayOf(1, 2, 3)
+    val list = listOf(1, 2, 3)
+    val array2 = arrayOf(1, 2L, "3")
+    val list2 = listOf(1, 2L, "3")
 
-fun max2(a : Int, b : Int) : Int {
-    return if (a > b) {
-        a
-    } else {
-        b
-    }
-}
+    array[0] = 3
 
-fun max3(a : Int, b : Int) = if (a > b) a else b
-
-// else 는 필수가 아니다.
-fun checkNum(score : Int) {
-    when (score) {
-        0 -> println("this is 0")
-        1 -> println("this is 1")
-        2,3 -> println("this is 2 or 3")
-        else -> println("I don't know")
-    }
-}
-
-// else 는 필수이다.
-fun checkNum2(score : Int) {
-    var a = when (score) {
-        1 -> 1
-        2 -> 2
-        else -> 3
-    }
-
-    println("a : ${a}")
-}
-
-fun checkNum3(score : Int) {
-    when (score) {
-        in 90..100 -> println("You are Good")
-        in 10..80 -> println("not bad")
-        else -> println("okay")
-    }
+    val arrayList = arrayListOf<Int>()
+    arrayList.add(10)
+    arrayList.add(20)
 }
