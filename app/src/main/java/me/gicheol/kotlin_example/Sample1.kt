418e9994f106c952a4849965fe3526772536debf
LeeGiCheol <asdqwek200@naver.com>
2021-12-19 21:45:48 +0900

    nullable, nonNull, let

--- app/src/main/java/me/gicheol/kotlin_example/Sample1.kt
+++ app/src/main/java/me/gicheol/kotlin_example/Sample1.kt
@@ -1,46 +1,40 @@
 package me.gicheol.kotlin_example
 
+import java.util.*
+
 fun main() {
-    forAndWhile()
+    nullCheck()
+    let()
 }
 
-fun forAndWhile() {
-    val users = arrayListOf("LEE", "GICHEOL", "ABC")
+// 타입을 생략하거나, ?를 붙이지 않으면 nonNull
+// ?를 붙이면 nullable, 타입 생략이 불가하다.
+// ?: Default 값을 지정할 수 있다.
+fun nullCheck() {
+    val name = "lee"
+    val nullName : String? = null
+    val nameInUpperCase = name.uppercase()
+    val nullNameInUpperCase = nullName?.uppercase()
+    println("nameInUpperCase = ${nameInUpperCase}")
+    println("nullNameInUpperCase = ${nullNameInUpperCase}")
 
-    for (user in users) {
-        println("user = ${user}")
-    }
+    val lastName : String? = null
+    val fullName = name + " " + (lastName?: "No lastname")
+    println("fullName = ${fullName}")
+}
 
-    for (i in 0..2) {
-        println("users[${i}] = ${users[i]}")
-    }
+// !!를 붙이면 해당 변수가 null이 아님을 컴파일러에게 알려준다.
+fun ignoreNulls(str : String?) {
+    val notNullVal : String = str!!
+    val upper = notNullVal.uppercase()
+}
 
-    var sum = 0
-    for (i in 1..10) {
-        sum += i
-    }
-
-    println("sum = ${sum}")
-
-    // 1 ~ 99 까지
-    sum = 0
-    for (i in 1 until 100) {
-        sum += i
-    }
-
-    println("sum = ${sum}")
-
-    for (i in 10 downTo 1) {
-        println("i = ${i}")
-    }
-
-    for ((index, name) in users.withIndex()) {
-        println("${index+1}번째 = ${name}")
-    }
-
-    var index = 0
-    while(index < 10) {
-        println("index = ${index}")
-        index++
+// 변수가 null이 아니라면 let 구문 실행
+// 변수명을 사용하거나, it 을 사용할 수 있다.
+fun let() {
+    val email : String? = "LEE@gmail.com"
+    email?.let {
+        println("email = ${email}")
+        println("it = ${it}")
     }
 }
